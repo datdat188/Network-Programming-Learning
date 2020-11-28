012e60b2bd8b1131566323947bb4e69ca1861335
datdat188 <tintl10999@gmail.com>
2020-11-28 20:48:00 +0700

    Clear obj file

--- np-hw2/1712328.c
+++ np-hw2/1712328.c
@@ -59,6 +59,11 @@ unsigned int *numberOfFilteredRequests;
 /// The total number of errored requests.
 unsigned int *numberOfErroredRequests;
 
+//===============================================
+void incrementNumberOfSuccessfulRequests();
+void incrementNumberOfFilteredRequests();
+void incrementNumberOfErroredRequests();
+
 //===========================UTILS FUNCTION===========================
 void error(char* msg)
 {
@@ -233,6 +238,34 @@ void handleSIGUSR2()
 }
 
 //=====================================================================
+/// Parse an HTTP request.
+/// @param request The HTTP request.
+/// @param error The possible error in processing.
+/// @return The parsed HTTP request.
+HTTPRequest processRequest(char *requestString, int *error);
+
+/// Determine whether a request should be filtered out or not.
+/// @param request The HTTP request.
+/// @return Whether or not the request should be allowed.
+bool shouldAllowRequest(HTTPRequest request);
+
+/// Determine whether a server should be filtered out or not.
+/// @param server The server in the HTTP request.
+/// @return Whether or not the server should be allowed.
+bool shouldAllowServer(const char *server);
+
+/// Send an HTTP Status Line to a socket.
+/// @param errorNo The HTTP Response code.
+/// @param client The socket to send the status to.
+/// @return Whether or not the transmission was successful.
+bool sendHTTPStatusToSocket(int status, int client);
+
+/// Handle an HTTP request.
+/// @param arg The request string.
+/// @return @c NULL
+/// @discussion Designed for use in new thread.
+void *handleRequest(void *arg);
+
 
 int main(int argc, char* argv[])
 {
@@ -314,22 +347,11 @@ int main(int argc, char* argv[])
 
             // Receive the message.
             bzero((char*)buffer,BUFFER_SIZE);
-			ssize_t n = recv(connfd, buffer, BUFFER_SIZE - 1, 0);
-			// Stream has errored or ended.
-			if (n <= 0)
-			{
-				// Errored.
-				perror("recv()");
-				continue;
-			}
-			else
-			{
-				// Stream received message.
-				buffer[n] = '\0';
-#ifdef DEBUG
-				//printf("Received message from fd %d: %s\n", fd, buffer);
-#endif
-			}
+			ssize_t n = Recv(connfd, buffer, BUFFER_SIZE, 0);
+			// Stream received message.
+			buffer[n] = '\0';
+		
+			//printf("Received message from fd %d\n: %s\n", listenfd, buffer);	
         /* 
 		 6. Your server does must be a concurrent server (i.e. do not use an iterative server).
 		 */
@@ -339,7 +361,7 @@ int main(int argc, char* argv[])
 			arg->sock = connfd;
 			arg->address = servaddr;
 			arg->msg = stringDuplicate(buffer);
-			//handleRequest(arg);
+			handleRequest(arg);
             exit(0);
 		}
 		Close(connfd);
@@ -347,3 +369,364 @@ int main(int argc, char* argv[])
     
     return EXIT_SUCCESS;
 }
+
+void *handleRequest(void *argument)
+{
+	// Unpack argument into variables.
+	sock_msg *arg = (sock_msg *)argument;
+	int fd = arg->sock;
+	struct sockaddr_in client = arg->address;
+	char *requestString = arg->msg;
+	// Process the request string into a HTTPRequest.
+	int error = 0;
+	HTTPRequest request = processRequest(requestString, &error);
+	if ( !request ) {
+		// Send back HTTP Error.
+		sendHTTPStatusToSocket(error, fd);
+		
+		// Increment number of (purposefully) errored requests.
+		incrementNumberOfErroredRequests();
+		
+		goto end;
+	}
+	
+	/*
+	 When your server detects a request that should be filtered, your server should return an HTTP error 403 (forbidden), which means you need to send back an HTTP status line that indicates an error.
+	 */
+	
+	// Read the IP Address into a string.
+	char *ip_addr = inet_ntoa((struct in_addr)client.sin_addr);
+	
+	// Figure out if the request should be filtered out.
+	if ( !shouldAllowRequest(request) ) {
+		// Print Request Line [FILTERED].
+		printf("%s: %s [FILTERED]\n", ip_addr, request[HTTPRequestHeaderField_Request_Line]);
+		
+		// Send back HTTP Error 403 Forbidden.
+		sendHTTPStatusToSocket(403, fd);
+		
+		// Increment filtered requests counter.
+		incrementNumberOfFilteredRequests();
+		goto end;
+	} else {
+		// Print Request Line.
+		printf("%s: %s\n", ip_addr, request[HTTPRequestHeaderField_Request_Line]);
+	}
+	
+	/*
+	 Your server must forward the appropriate HTTP request headers to the requested server, then send the responses back to the client.
+	 */
+	
+	int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if ( serverSocket < 0 ) {
+		perror("socket()");
+		goto end;
+	}
+	
+	// Server
+	struct sockaddr_in server;
+	server.sin_family = PF_INET;
+	
+	struct hostent *hp = gethostbyname(request[HTTPRequestHeaderField_Host]);
+	if ( hp == NULL ) {
+		perror("Unknown host");
+		goto end;
+	}
+	
+	// Could also use memcpy
+	bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+	unsigned short port = 80;
+	server.sin_port = htons(port);
+	
+	// Connect.
+	if ( connect(serverSocket, (struct sockaddr *)&server, sizeof(server) ) < 0 ) {
+		perror("connect()");
+		goto end;
+	}
+	
+	// Strip out Accept-Encoding to prevent chunking (not yet supported).
+	char *accept_encoding = request[HTTPRequestHeaderField_Accept_Encoding];
+	if ( accept_encoding != NULL ) {
+		free(accept_encoding);
+		request[HTTPRequestHeaderField_Accept_Encoding] = NULL;
+	}
+	
+	// Get the request string.
+	char *serverRequestString = requestStringFromRequest(request);
+	if ( !serverRequestString ) {
+		goto end;
+	}
+	
+	// Send.
+	ssize_t send_n = send(serverSocket, serverRequestString, strlen(serverRequestString), 0);
+	if ( send_n < strlen(serverRequestString) ) {
+		perror("send()");
+		goto end;
+	}
+	
+	// Buffer to load received messages into.
+	char buffer[BUFFER_SIZE];
+	
+	// Receive.
+	while (1) {
+		// BLOCK
+		ssize_t received_n = recv(serverSocket, buffer, BUFFER_SIZE - 1, 0);
+		if ( received_n == 0 ) {
+			// Peer has closed its half side of the (TCP) connection.
+			break;
+		} else if ( received_n < 0 ) {
+			// Error.
+			perror("recv()");
+			goto end;
+		} else {
+			// End the buffer with a null-terminator.
+			buffer[received_n] = '\0';
+			
+#ifdef DEBUG
+			// Print out the received message.
+			//printf("\n\nReceived message from %s:\n%s\n\n", inet_ntoa((struct in_addr)server.sin_addr), buffer);
+#endif
+			
+			ssize_t send_client_n = send(fd, buffer, strlen(buffer), 0);
+			if ( send_client_n < strlen(buffer) ) {
+				perror("send()");
+				goto end;
+			}
+		}
+	}
+	
+	// Increment number of successful requests.
+	incrementNumberOfSuccessfulRequests();
+		
+end:
+	
+	if ( requestString ) {
+		free(requestString);
+	}
+	
+	if ( serverRequestString ) {
+		free(serverRequestString);
+	}
+	
+	close(serverSocket);
+	
+	if ( request != NULL ) {
+		// Request is no longer needed.
+		HTTPRequestFree(request);
+	}
+	
+	// The socket is no longer needed.
+	close(fd);
+	
+	return NULL;
+}
+
+
+#pragma mark - Private API (Implementation)
+
+HTTPRequest processRequest(char *requestString, int *error)
+{
+	HTTPRequest request = HTTPRequestCreate();
+	
+	// Start the parsing at the beginning of the request.
+	char *parse = requestString;
+	static const char * const delimiter = "\r\n";
+	
+	// Parse out the rest of the headers.
+	for ( int i = 0; i < HTTPRequestHeaderFieldsCount; ++i ) {
+		
+		// Find the end of the line.
+		char *next = strstr(parse, delimiter);
+		printf("Count %d: \n%s", HTTPRequestHeaderFieldsCount, next);
+		
+		// Get just the line.
+		char *line = substring(parse, next);
+		
+		// Advance the parse pointer to the end of the line, and after the delimiter.
+		parse = next + strlen(delimiter);
+		
+		// Stop when at the end of the header.
+		if ( line == NULL ) {
+			break;
+		}
+		
+		// The first line is different than the rest.
+		if ( i == 0 ) {
+			
+			/*
+			 2. Your server must handle GET, HEAD, and POST request methods.
+			 */
+			
+			char *Method = NULL;
+			char *MethodArgs = NULL;
+			if ( !splitStringAtString(line, " ", &Method, &MethodArgs) ) {
+				// Invalid Request-Line.
+				*error = 400;
+				return false;
+			}
+			
+			char *Request_URI = NULL;
+			char *HTTP_Version = NULL;
+			if ( !splitStringAtString(MethodArgs, " ", &Request_URI, &HTTP_Version) ) {
+				// Invalid Request-Line.
+				*error = 400;
+				return false;
+			}
+			
+			unsigned long valueLength = strlen(Method) + 1 + strlen(Request_URI) + 1 + strlen(HTTP_Version) + 1;
+			char *value = malloc(sizeof(char) * valueLength);
+			strcpy(value, Method);
+			strcat(value, " ");
+			strcat(value, Request_URI);
+			strcat(value, " ");
+			strcat(value, HTTP_Version);
+			value[valueLength-1] = '\0';
+			
+			if ( stringEquality(Method, "GET") ) {
+				request[HTTPRequestHeaderField_Request_Line] = value;
+			} else if ( stringEquality(Method, "HEAD")) {
+				request[HTTPRequestHeaderField_Request_Line] = value;
+			} else if ( stringEquality(Method, "POST") ) {
+				request[HTTPRequestHeaderField_Request_Line] = value;
+			} else {
+				/*
+				 3. Your server must refuse to process any HTTP request method other than GET, HEAD, and POST. In such cases, you should send back an HTTP status code of 405 (Method not allowed) or 501 (Not Implemented) if you receive any other request method.
+				 */
+				HTTPRequestFree(request);
+				*error = 405;
+				return NULL;
+			}
+			
+		} else {
+			// In the format of: "Field Name: Value"
+			
+			// Split the line into field name and value.
+			char *fieldName = NULL;
+			char *fieldValue = NULL;
+			if ( !splitStringAtString(line, ": ", &fieldName, &fieldValue) ) {
+				// Line isn't in the correct format.
+				continue;
+			}
+			
+			HTTPRequestHeaderField field = HTTPRequestHeaderFieldForFieldNamed(fieldName);
+			if ( (int) field == -1 ) {
+				// Conversion failed.
+				continue;
+			}
+			request[field] = fieldValue;
+		}
+		
+#ifdef DEBUG
+		// Print the line.
+		//printf("%s\n", line);
+#endif
+	}
+	
+	/*
+	 Your server must send an error to the client whenever appropriate, including such cases as the request line being invalid, a Host: header is not found, or a POST request does not include a Content-Length: header. In these cases, your server must send a 400 (Bad Request) as a result.
+	 */
+	
+	// Check request to see if the request's valid.
+	if ( !validateRequest(request) ) {
+		// Free unused request.
+		HTTPRequestFree(request);
+		*error = 400;
+		return NULL;
+	}
+	
+	return request;
+}
+
+bool shouldAllowRequest(HTTPRequest request)
+{
+	// Request must be non-NULL.
+	if ( request == NULL ) {
+		return false;
+	}
+	
+	return shouldAllowServer(request[HTTPRequestHeaderField_Host]);
+}
+
+bool shouldAllowServer(const char *server)
+{
+	// The end of the server string (used for finding suffix matches).
+	const char *serverEnd = server + strlen(server);
+	
+	// Check against all of the filters.
+	for ( int i=0; i<*filtersCount; ++i ) {
+		const char *filter = filters[i];
+		
+		// Length of filter string.
+		size_t filterLen = strlen(filter);
+		
+		// Check prefix.
+		if ( caseInsensitiveStringComparisonLimited(server, filter, filterLen) == 0 ) {
+			return false;
+		}
+		
+		// Check suffix.
+		if ( caseInsensitiveStringComparisonLimited(serverEnd - filterLen, filter, filterLen) == 0 ) {
+			return false;
+		}
+		
+	}
+	
+	return true;
+}
+
+bool sendHTTPStatusToSocket(int status, int client)
+{
+	// Find the maximum length of the status string. (To create buffer of correct size).
+	unsigned long maxLength =  8  // "HTTP/1.1"
+						    +  1
+							+  3  // 3-digit status code.
+						    +  1
+							+ 36  // Max length of status string
+	                        +  5; // Double carriage return/newline + null-terminator
+	
+	// Create buffer for status string.
+	char *status_str = malloc(sizeof(char) * maxLength);
+	
+	char *HTTP_Version = "HTTP/1.1";
+	
+	// Populate the string.
+	int sprinted = snprintf(status_str, maxLength, "%s %d %s\r\n\r\n", HTTP_Version, status, statusStringForStatusCode(status));
+	
+	// Check for errors in sprintf.
+	if ( sprinted < 0 ) {
+		// Error in snprintf.
+	} else if ( sprinted < maxLength ) {
+		// Did not get to sprint the whole string.
+	}
+	
+	// Send the status.
+	ssize_t send_n = send(client, status_str, strlen(status_str), 0);
+	
+	
+	// Check for errors in send.
+	if ( send_n < strlen(status_str) ) {
+		perror("send()");
+		return false;
+	}
+	
+	// The string is no longer needed.
+	free(status_str);
+
+	// Everything went OK!
+	return true;
+}
+
+void incrementNumberOfSuccessfulRequests()
+{
+	(*numberOfSuccessfulRequests)++;
+}
+
+void incrementNumberOfFilteredRequests()
+{
+	(*numberOfFilteredRequests)++;
+}
+
+void incrementNumberOfErroredRequests()
+{
+	(*numberOfErroredRequests)++;
+}	
